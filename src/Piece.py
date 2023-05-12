87847481602b54a51081de274b57f7c28129c0e8
Janik Teune <janik.teune@st.ovgu.de>
2023-05-12 22:15:58 +0200

    fix type annotations, damit nossel nicht rumheult

--- src/Piece.py
+++ src/Piece.py
@@ -1,19 +1,25 @@
+from typing import Tuple
+
 from src.Board import Board
 
 
 class Piece:
+    position: Tuple[int, int]
 
-    position: (int, int)
-
-    def generate_moves(self, board: Board) -> [(int, int)]:
+    def generate_moves(self, board: Board) -> [Tuple[int, int]]:
         pseudo_moves = self.generate_pseudo_legal_moves(board)
         moves = [move for move in pseudo_moves if self.is_legal_move(move, board)]
         return moves
 
-    def generate_pseudo_legal_moves(self, board: Board) -> [(int, int)]:
+    def generate_pseudo_legal_moves(self, board: Board) -> [Tuple[int, int]]:
         pass
 
-    def is_legal_move(self, move: (int, int), board: Board) -> bool:
-        pass
+    def is_legal_move(self, move_to: Tuple[int, int], board: Board) -> bool:
+        x, y = move_to
+        if x < 0 or x > 7 or y < 0 or y > 7:
+            return False
+
+        return True
+
         # newBoard: apply move on board
         # board.is_valid
