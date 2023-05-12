336194817827378a6fc9a4d62e3df53f5a768c41
MrUnkn0wn <mrunkioiw@gmail.com>
2023-05-12 23:05:12 +0200

    Added TextureLoader.py

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,12 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
+from src.pieces.TextureLoader import TextureLoader
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
-running = True
-
 
 def draw_board(size_in_pixels):
     for x in range(8):
@@ -20,23 +20,27 @@ def draw_board(size_in_pixels):
             pygame.draw.rect(screen, color, (size_in_pixels * x, size_in_pixels * y,
                                              size_in_pixels * (x + 1), size_in_pixels * (y + 1)))
 
+def main_loop():
+    running = True
+    loader = TextureLoader()
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("gray")
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("gray")
+        # RENDER YOUR GAME HERE
+        draw_board(75)
 
-    # RENDER YOUR GAME HERE
-    draw_board(75)
+        # flip() the display to put your work on screen
+        pygame.display.flip()
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        clock.tick(60)  # limits FPS to 60
 
-    clock.tick(60)  # limits FPS to 60
+    pygame.quit()
 
-pygame.quit()
+main_loop()
--- src/pieces/TextureLoader.py
+++ src/pieces/TextureLoader.py
@@ -3,4 +3,37 @@ import pygame
 
 class TextureLoader:
     def __init__(self):
-        self.texture_image = pygame.image.load('assets/pieces/WhitePieces-Sheet.png')
+        self.texture_image = pygame.image.load('/home/mrunkn0wn/Documents/Code/BloodySwapChess/assets/pieces/spriteSheet.png').convert_alpha()
+        mask = pygame.mask.from_surface(self.texture_image)
+        rects = mask.get_bounding_rects()
+
+        self.textures = []
+        for rect in rects:
+            self.textures.append(self.texture_image.subsurface(rect))
+
+    def get_texture(self, piece:str, white:bool):
+        match piece:
+            case "king":
+                if white:
+                    return self.textures[6]
+                return self.textures[0]
+            case "queen":
+                if white:
+                    return self.textures[7]
+                return self.textures[1]
+            case "rook":
+                if white:
+                    return self.textures[8]
+                return self.textures[2]
+            case "bishop":
+                if white:
+                    return self.textures[9]
+                return self.textures[3]
+            case "knight":
+                if white:
+                    return self.textures[10]
+                return self.textures[4]
+            case "pawn":
+                if white:
+                    return self.textures[11]
+                return self.textures[5]
